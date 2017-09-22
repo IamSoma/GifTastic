6572d2b3df786945d2496cf2ba5a34b56e752a2f
Nick Grismore <nicksk83@aol.com>
2017-09-21 21:48:46 -0400

    added app.js folder and added some bootstrap

---
+++ app.js
@@ -0,0 +1,9 @@
+var topics = ["space", "stars", "planets", "spaceships", "aliens", "spacebattles"];
+
+
+
+for (var i = topics.length - 1; i >= 0; i--) {
+	console.log(topics[i]);
+}
+		
+$(.buttons)append.(topics);
