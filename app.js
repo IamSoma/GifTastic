0483f5fb70a13c7860b011e6c886437c25419d81
Nick Grismore <nicksk83@aol.com>
2017-09-25 21:26:08 -0400

    added for loop

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var topics = ["space", "stars", "planets", "spaceships", "aliens", "spacebattles
 
 
 
-for (var i = topics.length - 1; i >= 0; i--) {
+for (var i = 0; topics.length - 1; i >= 0; i--) {
 	console.log(topics[i]);
 }
 		
-$(.buttons)append.(topics);
