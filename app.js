6c1a8858ba182ed8e2b0f5eb9924bc95350277a0
Nick Grismore <nicksk83@aol.com>
2017-09-29 23:26:44 -0400

    updates

--- app.js
+++ app.js
@@ -1,48 +1,36 @@
-  $(function(){
-    topicsButtons(topics,'searchNames', '.buttons');
+$(window).load(function(){
+  topicsButtons(topics,'searchNames', '.buttons');
+  $('.topicButton').on('click', function(){
+    alert('event handlerworking');
   })
+
+})
 
   var topics = ["space", "stars", "planets", "spaceships", "aliens", "spacebattles"];
 
 function topicsButtons(topics, classAdder, buttonAdder){
     $(buttonAdder).empty();
 	for (var i=0; i<topics.length; i++) {
-      var a = $('<button>');  
-      a.addClass("btn btn-primary");      
-      a.attr("data-type", topics[i]);     
-      a.text(topics[i]);
+      var myElement = $('<button>');  
+      myElement.addClass("topicButton");      
+      myElement.attr("data-type", topics[i]);     
+      myElement.text(topics[i]);
       $(buttonAdder).append(a);
-      $("#buttons-view").append(a);
+      // $("#buttons-view").append(a);
      }
   }
-
-    // Event listener for our space-button
-    $("#space-button").on("click", function() {
-
-      // Storing our giphy API URL for a random space image
-      var key = "Wlr6m0r2PKy2X6cGhYt3xRpFVa01vrAC&";
-      var queryURL = "http://api.giphy.com/v1/gifs/random?api_key="+ key +"tag="+ topics[1];
-
-
-      // Perfoming an AJAX GET request to our queryURL
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).done(function(res) {
-      	console.log(res);
-
-        // Saving the image_original_url property
-        var imageUrl = res.data.image_original_url;
-
-        // Creating and storing an image tag
-        var spaceImage = $("<img>");
-
-        // Setting the catImage src attribute to imageUrl
-        spaceImage.attr("src", imageUrl);
-        spaceImage.attr("alt", "space image");
-
-        // Prepending the spaceImage to the images div
-        $(".images").prepend(spaceImage);
-      });
-    });
+    // $("#button").on("click", function() {
+    //   var key = "Wlr6m0r2PKy2X6cGhYt3xRpFVa01vrAC&";
+    //   var queryURL = "http://api.giphy.com/v1/gifs/random?api_key="+ key +"tag="+ topics[1];
+    //   $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //   }).done(function(res) {
+    //     var imageUrl = res.data.image_original_url;
+    //     var spaceImage = $("<img>");
+    //     spaceImage.attr("src", imageUrl);
+    //     spaceImage.attr("alt", "space image");
+    //     $(".images").prepend(spaceImage);
+    //   });
+    // });
 
